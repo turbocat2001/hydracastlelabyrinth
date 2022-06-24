8d9d4188db308f238fa78df01c13e2f28de4c640
turbocat2001 <maximlogaev2001ezro@gmail.com>
2022-06-24 23:17:44 +0300

    added getenv emulation

--- src/misc.c
+++ src/misc.c
@@ -8,3 +8,13 @@ void *memrchr(const void *m, int c, size_t n)
     while (n--) if (s[n]==c) return (void *)(s+n);
     return 0;
 }
+
+const char* home_dir = ".";
+
+char *getenv(char* env_name)
+{
+    if (!strcmp(env_name, "HOME")) {
+        return (char*)home_dir;
+    }
+    return NULL;
+}
